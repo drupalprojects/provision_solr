416efe99d574a1968d347e2f84cb14470ebcf812
Jacinto Capote <aegir@localhost.localdomain>
2012-09-14 23:01:35 +0200

    Implemeted new module with deploy tomcat6

---
+++ lucene/solr.tpl.php
@@ -0,0 +1,3 @@
+<Context docBase="<?php print $warn_file; ?>" debug="0" privileged="true" allowLinking="true" crossContext="true">
+  <Environment name="<?php print $solr_core_name; ?>/home" type="java.lang.String" value="<? print $home_directory; ?>" override="true" />
+  </Context>
