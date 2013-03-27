edb4190b2b3ccaa25f623729c90a68022f0ca9b2
Jacinto Capote Robles <jacintocapote@gmail.com>
2013-03-27 16:41:42 +0100

    Jacinto. Fixed a little problem with create sites

--- lucene/solr.tpl.php
+++
@@ -1,3 +0,0 @@
-<Context docBase="/usr/share/tomcat6/webapps/solr.war" debug="0" privileged="true" allowLinking="true" crossContext="true">
-  <Environment name="solr/home" type="java.lang.String" value="<? print $home_directory; ?>" override="true" />
-  </Context>
