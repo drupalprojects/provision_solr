89ba2855e4829d0abb63feabb3c151427ba5fa1e
Jacinto Capote Robles <jacintocapote@gmail.com>
2013-03-27 17:24:46 +0100

    Jacinto. Added Provision/lucene

---
+++ Provision/lucene/solr.tpl.php
@@ -0,0 +1,3 @@
+<Context docBase="/usr/share/tomcat6/webapps/solr.war" debug="0" privileged="true" allowLinking="true" crossContext="true">
+  <Environment name="solr/home" type="java.lang.String" value="<? print $home_directory; ?>" override="true" />
+  </Context>
