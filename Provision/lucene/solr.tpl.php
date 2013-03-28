5444b7bb194fe9b7dacf31299845ff5dbbe28f69
Jon Pugh <jon@thinkdrop.net>
2013-03-28 13:22:34 -0400

    -setting solr_path_to_war -refactoring setup, now copies solr conf folder

--- Provision/lucene/solr.tpl.php
+++ Provision/lucene/solr.tpl.php
@@ -1,3 +1,3 @@
-<Context docBase="/usr/share/tomcat6/webapps/solr.war" debug="0" privileged="true" allowLinking="true" crossContext="true">
-  <Environment name="solr/home" type="java.lang.String" value="<? print $home_directory; ?>" override="true" />
+<Context docBase="<?php print $path_to_solr_war; ?>" debug="0" privileged="true" allowLinking="true" crossContext="true">
+  <Environment name="solr/home" type="java.lang.String" value="<?php print $home_directory; ?>" override="true" />
   </Context>
