830f32eac35690652467d3678151178b92131eef
Jon Pugh <jon@thinkdrop.net>
2013-04-17 12:51:02 -0400

    changing solr war path var

--- Provision/lucene/solr.tpl.php
+++ Provision/lucene/solr.tpl.php
@@ -1,3 +1,3 @@
-<Context docBase="<?php print $path_to_solr_war; ?>" debug="0" privileged="true" allowLinking="true" crossContext="true">
+<Context docBase="<?php print $solr_war_path; ?>" debug="0" privileged="true" allowLinking="true" crossContext="true">
   <Environment name="solr/home" type="java.lang.String" value="<?php print $home_directory; ?>" override="true" />
   </Context>
