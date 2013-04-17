3f339bdd6800ef7310c631923df299e2d9634f57
Jon Pugh <jon@thinkdrop.net>
2013-04-17 19:32:53 -0400

    refactor based on http > apache model

---
+++ tomcat/site.tpl.php
@@ -0,0 +1,3 @@
+<Context docBase="<?php print $solr_war_path; ?>" debug="0" privileged="true" allowLinking="true" crossContext="true">
+  <Environment name="solr/home" type="java.lang.String" value="<?php print $solr_home_path; ?>" override="true" />
+</Context>
