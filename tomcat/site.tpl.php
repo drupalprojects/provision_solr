2f588d3edc170d927b4ed4eec518dbcd3acac171
Jon Pugh <jon@thinkdrop.net>
2013-04-18 10:52:07 -0400

    adding solr home to the template vars

--- tomcat/site.tpl.php
+++ tomcat/site.tpl.php
@@ -1,4 +1,4 @@
 <!-- <?php print $comment; ?>-->
 <Context docBase="<?php print $solr_war_path; ?>" debug="0" privileged="true" allowLinking="true" crossContext="true">
-  <Environment name="solr/home" type="java.lang.String" value="<?php print $solr_home_path; ?>" override="true" />
+  <Environment name="solr/home" type="java.lang.String" value="<?php print $solr_home; ?>" override="true" />
 </Context>
