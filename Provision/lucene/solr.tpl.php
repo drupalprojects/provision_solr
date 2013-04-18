e1925a3babefbf2b8e1efca35c2d9d7b7145017c
Jon Pugh <jon@thinkdrop.net>
2013-04-18 11:20:48 -0400

    removing old provision lucene code

--- Provision/lucene/solr.tpl.php
+++
@@ -1,3 +0,0 @@
-<Context docBase="<?php print $solr_war_path; ?>" debug="0" privileged="true" allowLinking="true" crossContext="true">
-  <Environment name="solr/home" type="java.lang.String" value="<?php print $home_directory; ?>" override="true" />
-  </Context>
