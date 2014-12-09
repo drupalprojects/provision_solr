ab9fdafbbb259bc5afbabbec1ab3a524cb094970
Jon Pugh <jon@thinkdrop.net>
2014-12-09 12:38:29 -0500

    Moving tomcat config and service to provision 2.x folders.

---
+++ Provision/Config/Tomcat/Server.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Apache server level configuration file class
+ */
+class Provision_Config_Tomcat_Server extends Provision_Config_Solr_Server {
+  protected $mode = 0755;
+
+  function process() {
+    parent::process();
+  }
+
+  function write() {
+    parent::write();
+
+    // Ensure chmod of the tomcat folder
+    provision_file()->chmod($this->filename(), $this->mode);
+  }
+}
---
+++ Provision/Config/Tomcat/Site.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Solr site level config class.
+ */
+class Provision_Config_Tomcat_Site extends Provision_Config_Solr_Site {
+  function process() {
+    parent::process();
+  }
+}
