b033b5d507d4845f48b5acb30051c7794cba1b62
Jon Pugh <jon@thinkdrop.net>
2014-12-09 12:47:50 -0500

    adding verify commands.

--- Provision/Service/solr.php
+++ Provision/Service/solr.php
@@ -6,6 +6,21 @@
 class Provision_Service_solr extends Provision_Service {
   public $service = 'solr';
 
+  function verify_server_cmd() {
+    $this->create_config($this->context->type);
+    $this->parse_configs();
+  }
+
+  function verify_platform_cmd() {
+    $this->create_config($this->context->type);
+    $this->parse_configs();
+  }
+
+  function verify_site_cmd() {
+    $this->create_config($this->context->type);
+    $this->parse_configs();
+  }
+
   /**
    * Add the civicrm_version property to the site context.
    */
