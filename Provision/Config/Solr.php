cebe9e1dad59c7d13d7c9fd3f4324f16ec1bfe38
Jon Pugh <jon@thinkdrop.net>
2015-04-14 16:57:36 -0400

    Sync the entire solr home folder to the server.

--- Provision/Config/Solr.php
+++ Provision/Config/Solr.php
@@ -9,11 +9,11 @@
 class Provision_Config_Solr extends Provision_Config {
   function write() {
     parent::write();
-    $this->data['server']->sync($this->filename());
+    $this->data['server']->sync($this->data['solr_home']);
   }
 
   function unlink() {
     parent::unlink();
-    $this->data['server']->sync($this->filename());
+    $this->data['server']->sync($this->data['solr_home']);
   }
 }
